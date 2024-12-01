689c6067900a6486dc2e2bc10899662e1e9bf289
Quinn Shanahan <q.shanahan@gmail.com>
2024-12-01 06:59:10 -0500

    plugin interface (clean)

--- cmd/plugins/gmail/main.go
+++ cmd/plugins/gmail/main.go
@@ -1,66 +1,10 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"log"
-	"os"
-
-	pb "go.quinn.io/dataq/proto"
-	"google.golang.org/protobuf/encoding/protojson"
+	"go.quinn.io/dataq/pkg/pluginutil"
 )
 
 func main() {
 	plugin := New()
-
-	// Read request from stdin
-	input, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading stdin: %v\n", err)
-		os.Exit(1)
-	}
-
-	log.Printf("Received request: %s", string(input))
-
-	// Parse request
-	var req pb.PluginRequest
-	if err := protojson.Unmarshal(input, &req); err != nil {
-		fmt.Fprintf(os.Stderr, "Error parsing request: %v\n", err)
-		os.Exit(1)
-	}
-
-	log.Printf("Parsed request: %+v", req)
-
-	// Process request
-	var resp pb.PluginResponse
-	resp.PluginId = plugin.ID()
-
-	// Always configure the plugin first
-	if err := plugin.Configure(req.Config); err != nil {
-		resp.Error = err.Error()
-	} else if req.Operation == "extract" {
-		items, err := plugin.Extract(context.Background())
-		if err != nil {
-			resp.Error = err.Error()
-		} else {
-			for item := range items {
-				resp.Items = append(resp.Items, item)
-			}
-		}
-	} else if req.Operation != "configure" {
-		resp.Error = fmt.Sprintf("unknown operation: %s", req.Operation)
-	}
-
-	// Write response
-	output, err := protojson.Marshal(&resp)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error marshaling response: %v\n", err)
-		os.Exit(1)
-	}
-
-	if _, err := os.Stdout.Write(output); err != nil {
-		fmt.Fprintf(os.Stderr, "Error writing response: %v\n", err)
-		os.Exit(1)
-	}
+	pluginutil.HandlePlugin(plugin)
 }
---
+++ pkg/pluginutil/plugin.go
@@ -0,0 +1,68 @@
+package pluginutil
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	pb "go.quinn.io/dataq/proto"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+// Plugin is the interface that all plugins must implement
+type Plugin interface {
+	ID() string
+	Configure(map[string]string) error
+	Extract(context.Context) (<-chan *pb.DataItem, error)
+}
+
+// HandlePlugin reads the plugin request from stdin, processes it using the given plugin,
+// and writes the response to stdout
+func HandlePlugin(p Plugin) {
+	// Read request from stdin
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading stdin: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Parse request
+	var req pb.PluginRequest
+	if err := protojson.Unmarshal(input, &req); err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing request: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Process request
+	var resp pb.PluginResponse
+	resp.PluginId = p.ID()
+
+	// Always configure the plugin first
+	if err := p.Configure(req.Config); err != nil {
+		resp.Error = err.Error()
+	} else if req.Operation == "extract" {
+		items, err := p.Extract(context.Background())
+		if err != nil {
+			resp.Error = err.Error()
+		} else {
+			for item := range items {
+				resp.Items = append(resp.Items, item)
+			}
+		}
+	} else if req.Operation != "configure" {
+		resp.Error = fmt.Sprintf("unknown operation: %s", req.Operation)
+	}
+
+	// Write response to stdout
+	output, err := protojson.Marshal(&resp)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error marshaling response: %v\n", err)
+		os.Exit(1)
+	}
+
+	if _, err := os.Stdout.Write(output); err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing response: %v\n", err)
+		os.Exit(1)
+	}
+}
