40c60d1552e8ef4aba894c1318ee701908f3a728
Quinn Shanahan <q.shanahan@gmail.com>
2024-12-03 18:36:38 -0500

    refactor

--- stream/stream.go
+++ stream/stream.go
@@ -7,9 +7,63 @@ import (
 
 	pb "go.quinn.io/dataq/proto"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 const LengthSize = 8 // 8-byte length header
+
+// ReadRequest reads a PluginRequest from the reader using length-prefixed framing
+func Read[T protoreflect.ProtoMessage](r io.Reader, msg T) error {
+	// Read length header (8 bytes, big endian)
+	var length uint64
+	err := binary.Read(r, binary.BigEndian, &length)
+	if err != nil {
+		if err == io.EOF {
+			return nil
+		}
+		return fmt.Errorf("failed to read length header: %w", err)
+	}
+
+	// Read data
+	data := make([]byte, length)
+	_, err = io.ReadFull(r, data)
+	if err != nil {
+		return fmt.Errorf("failed to read data: %w", err)
+	}
+
+	// Unmarshal the PluginRequest
+	var req T
+	err = proto.Unmarshal(data, req)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal PluginRequest: %w", err)
+	}
+
+	return nil
+}
+
+// WriteRequest writes a PluginRequest to the writer using length-prefixed framing
+func Write[T protoreflect.ProtoMessage](w io.Writer, msg T) error {
+	// Marshal the PluginRequest
+	data, err := proto.Marshal(msg)
+	if err != nil {
+		return fmt.Errorf("failed to marshal PluginRequest: %w", err)
+	}
+
+	// Write length header (8 bytes, big endian)
+	length := uint64(len(data))
+	err = binary.Write(w, binary.BigEndian, length)
+	if err != nil {
+		return fmt.Errorf("failed to write length header: %w", err)
+	}
+
+	// Write data
+	_, err = w.Write(data)
+	if err != nil {
+		return fmt.Errorf("failed to write data: %w", err)
+	}
+
+	return nil
+}
 
 // WriteRequest writes a PluginRequest to the writer using length-prefixed framing
 func WriteRequest(w io.Writer, req *pb.PluginRequest) error {
